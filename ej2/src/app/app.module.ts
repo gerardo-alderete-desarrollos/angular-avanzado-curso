8529adc506666fc8c8b9302840c01ca102e47d9a
Gerardo Alderete Flores <galderetef@coltomex.bpo>
2019-01-16 10:31:01 -0600

    EJERCICIO 2

--- ej2/src/app/app.module.ts
+++ ej2/src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about.component';
-import { MainComponent } from './pages/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent,
-    MainComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule
