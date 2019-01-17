e1c88a9f2a5c385664699e0630429b71494fb799
Gerardo Alderete <gerardoalderete92@gmail.com>
2019-01-16 23:21:11 -0600

    implementando rutas principales

--- adminpro/src/app/app.module.ts
+++ adminpro/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// RUTAS
+import { APP_ROUTES } from './app.routes';
+
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
@@ -24,7 +28,8 @@ import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component
     BreadcrumbsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
