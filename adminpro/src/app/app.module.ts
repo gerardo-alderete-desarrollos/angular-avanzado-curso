bc6cebd93dbff33b757bbb6b12690da82c66986f
Gerardo Alderete <gerardoalderete92@gmail.com>
2019-01-17 22:57:22 -0600

    Separando el login del template

--- adminpro/src/app/app.module.ts
+++ adminpro/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component
     Graficas1Component,
     HeaderComponent,
     SidebarComponent,
-    BreadcrumbsComponent
+    BreadcrumbsComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
